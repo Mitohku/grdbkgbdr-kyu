66aa4e4eeb3aad3757b303a90c2d3f8e388c0532
Mitohku <34718938+Mitohku@users.noreply.github.com>
2017-12-23 14:57:23 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -960,6 +960,52 @@ async def advert(ctx):
     await adv1.send(embed = embed)
     await ctx.send(ctx.message.author, "Thanks for your advertisement!")        
         
+@bot.command()
+async def cl(self):
+
+    data = json.load(open('cogs/data/changelogs.json'))
+
+    logs_author         = self.bot.get_user(self.bot.owner_id)
+    json_log_date       = data["data"]["changelogs"][0]["date"]
+    json_log_added      = data["data"]["changelogs"][0]["added"]
+    json_log_removed    = data["data"]["changelogs"][0]["removed"]
+    json_log_changed    = data["data"]["changelogs"][0]["changed"]
+    json_log_fixed      = data["data"]["changelogs"][0]["fixed"]
+    json_log_extra      = data["data"]["changelogs"][0]["extra"]
+
+    embed = discord.Embed(colour = 0xA522B3)
+    embed.set_thumbnail(url = f"{self.bot.user.avatar_url}")
+    embed.set_author(name = f"Changelog: {json_log_date}.", icon_url = f"{self.bot.user.avatar_url}")
+    embed.set_footer(text = f"Created by {logs_author.name} on {json_log_date}", icon_url = f"{logs_author.avatar_url}")
+
+    if json_log_added:
+        embed.add_field(name = "Added:", value = f"{json_log_added}", inline=False)
+    else:
+        pass
+
+    if json_log_removed:
+        embed.add_field(name = "Removed: ", value = f"{json_log_removed}", inline=False)
+    else:
+        pass
+
+    if json_log_changed:
+        embed.add_field(name = "Changed: ", value = f"{json_log_changed}", inline=False)
+    else:
+        pass
+
+    if json_log_fixed:
+        embed.add_field(name = "Fixed: ", value = f"{json_log_fixed}", inline=False)
+    else:
+        pass
+
+    if json_log_extra:
+        embed.add_field(name = "Extra Information: ", value = f"{json_log_extra}", inline=False)
+    else:
+        pass
+
+    await self.message.delete()
+    await Channel.send((390546585161039872), f"@everyone, heyy there is a new update!")
+    await Channel.send((390546585161039872), embed=embed)
 
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 if not os.environ.get('TOKEN'):
