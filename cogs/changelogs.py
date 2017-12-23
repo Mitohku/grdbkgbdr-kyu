1b222856c477c8ff9b4154a9c95e94527670b6d0
Mitohku <34718938+Mitohku@users.noreply.github.com>
2017-12-23 13:46:21 +0100

    Create changelogs.py

---
+++ cogs/changelogs.py
@@ -0,0 +1,127 @@
+###########################################
+############ Changelog Command ############
+###########################################
+
+import discord
+import random
+import json
+from config import *
+from discord.ext import commands
+from config import embed_color, embed_color_succes, embed_color_error
+
+class Changelog():
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.group(invoke_without_command = True, aliases=['changelog', 'updates', 'changes'])
+    async def changelogs(self, ctx):
+        data = json.load(open('cogs/data/changelogs.json'))
+        
+        logs_author         = self.bot.get_user(self.bot.owner_id)
+        json_log_date       = data["data"]["changelogs"][0]["date"]
+        json_log_version    = data["data"]["changelogs"][0]["version"]
+        json_log_added      = data["data"]["changelogs"][0]["added"]
+        json_log_removed    = data["data"]["changelogs"][0]["removed"]
+        json_log_changed    = data["data"]["changelogs"][0]["changed"]
+        json_log_fixed      = data["data"]["changelogs"][0]["fixed"]
+        json_log_extra      = data["data"]["changelogs"][0]["extra"]
+
+        embed = discord.Embed(colour = embed_color)
+        embed.set_thumbnail(url = f"{self.bot.user.avatar_url}")
+        embed.set_author(name = f"Changelog: {json_log_date}.", icon_url = f"{self.bot.user.avatar_url}")
+        embed.set_footer(text = f"Created by {logs_author.name} on {json_log_date}", icon_url = f"{logs_author.avatar_url}")
+        embed.add_field(name = "Version: ", value = f"{json_log_version}", inline=False)
+
+        if json_log_added:
+            embed.add_field(name = "Added:", value = f"{json_log_added}", inline=False)
+        else:
+            pass
+
+        if json_log_removed:
+            embed.add_field(name = "Removed: ", value = f"{json_log_removed}", inline=False)
+        else:
+            pass
+
+        if json_log_changed:
+            embed.add_field(name = "Changed: ", value = f"{json_log_changed}", inline=False)
+        else:
+            pass
+
+        if json_log_fixed:
+            embed.add_field(name = "Fixed: ", value = f"{json_log_fixed}", inline=False)
+        else:
+            pass
+
+        if json_log_extra:
+            embed.add_field(name = "Extra Information: ", value = f"{json_log_extra}", inline=False)
+        else:
+            pass
+
+        try:
+            await ctx.message.delete()
+        except:
+            pass        
+        await ctx.send(embed=embed)
+
+    @changelogs.command(name = 'tag')
+    async def changelogs_tag(self, ctx, *, role : discord.Role = None):
+
+        if not role:
+            try:
+                await ctx.message.delete()
+            except:
+                pass
+            return
+
+        else:
+            data = json.load(open('cogs/data/changelogs.json'))
+
+            logs_author         = self.bot.get_user(self.bot.owner_id)
+            json_log_date       = data["data"]["changelogs"][0]["date"]
+            json_log_version    = data["data"]["changelogs"][0]["version"]
+            json_log_added      = data["data"]["changelogs"][0]["added"]
+            json_log_removed    = data["data"]["changelogs"][0]["removed"]
+            json_log_changed    = data["data"]["changelogs"][0]["changed"]
+            json_log_fixed      = data["data"]["changelogs"][0]["fixed"]
+            json_log_extra      = data["data"]["changelogs"][0]["extra"]
+
+            embed = discord.Embed(colour = embed_color)
+            embed.set_thumbnail(url = f"{self.bot.user.avatar_url}")
+            embed.set_author(name = f"Changelog: {json_log_date}.", icon_url = f"{self.bot.user.avatar_url}")
+            embed.set_footer(text = f"Created by {logs_author.name} on {json_log_date}", icon_url = f"{logs_author.avatar_url}")
+            embed.add_field(name = "Version: ", value = f"{json_log_version}", inline=False)
+
+            if json_log_added:
+                embed.add_field(name = "Added:", value = f"{json_log_added}", inline=False)
+            else:
+                pass
+
+            if json_log_removed:
+                embed.add_field(name = "Removed: ", value = f"{json_log_removed}", inline=False)
+            else:
+                pass
+
+            if json_log_changed:
+                embed.add_field(name = "Changed: ", value = f"{json_log_changed}", inline=False)
+            else:
+                pass
+
+            if json_log_fixed:
+                embed.add_field(name = "Fixed: ", value = f"{json_log_fixed}", inline=False)
+            else:
+                pass
+
+            if json_log_extra:
+                embed.add_field(name = "Extra Information: ", value = f"{json_log_extra}", inline=False)
+            else:
+                pass
+
+            try:
+                await ctx.message.delete()
+            except:
+                pass
+            await ctx.send(f"<:Noella:384046588345188352> | {role.mention}, yeey there is a new update!")
+            await ctx.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(Changelog(bot))
